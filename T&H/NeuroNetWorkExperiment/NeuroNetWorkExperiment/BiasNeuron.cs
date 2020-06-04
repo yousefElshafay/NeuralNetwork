89b35645842a230f3a71b0770d15543976be23b5
yousefElshafay <yousefmohamed1993@yahoo.com>
2020-06-04 18:55:51 +0200

    Update BiasNeuron.cs

--- T&H/NeuroNetWorkExperiment/NeuroNetWorkExperiment/BiasNeuron.cs
+++ T&H/NeuroNetWorkExperiment/NeuroNetWorkExperiment/BiasNeuron.cs
@@ -13,16 +13,13 @@ namespace NeuroNetWorkExperiment
        // Dictionary<IObserver, double> OutPutNeuron;
 
         public BiasNeuron()
-        {
-           
+        {  
             OutPutNeuron = new Dictionary<IObserver, double>();
         }
         
-
         public void update(double InputVal, double ConnectionWeight)//inputVal will be ignored since this is a biasNeuron
         {
             this.connectionWeight = ConnectionWeight;
-
         }
 
         //public void AddNeuron(IObserver newNeuron, double ConnectionWeight)
@@ -30,23 +27,18 @@ namespace NeuroNetWorkExperiment
         //    OutPutNeuron.Add(newNeuron, ConnectionWeight);
         //}
 
-
         //public void RemoveNeuron(IObserver SelectedNeuron)
         //{
         //    OutPutNeuron.Remove(SelectedNeuron);
         //}
-
 
         public override void Fire()
         {
             foreach (KeyValuePair<IObserver, double> Neuron in OutPutNeuron)
             {
                 Neuron.Key.update(outputVal, Neuron.Value);// this 0.1 should be the result of the sigmoid function 
-
                 Console.WriteLine(Neuron.Key.ToString() + " bias neuron key //" + Neuron.Value.ToString());// for debugging purpose  
-
             }
-         
         }
     }
 }
